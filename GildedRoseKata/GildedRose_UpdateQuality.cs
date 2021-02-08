0d62e2c277053d83e19569a5d111b587b9451087
Steve Smith <steve@kentsmiths.com>
2021-02-08 11:56:30 -0500

    Refactor to add solution Update nugets Add fluentassertions and shouldly

---
+++ GildedRoseKata/GildedRose_UpdateQuality.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Shouldly;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace GildedRoseKata
+{
+    /// <summary>
+    /// Test naming convention recommendation:
+    /// https://ardalis.com/unit-test-naming-convention/
+    /// </summary>
+    public class GildedRose_UpdateQuality
+    {
+        [Fact]
+        public void DoesNothingGivenSulfuras()
+        {
+            int initialQuality = 80;
+            var items = new List<Item> {
+                                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = initialQuality},
+
+            };
+            var gildedRose = new GildedRose(items);
+            
+            gildedRose.UpdateQuality();
+
+            var firstItem = items.First();
+            
+            // Use your preferred assertion library (already included - pick one delete others)
+            // xunit default
+            Assert.Equal(initialQuality, firstItem.Quality);
+
+            // fluentassertions
+            firstItem.Quality.Should().Be(initialQuality);
+
+            // shouldly
+            firstItem.Quality.ShouldBe(initialQuality);
+        }
+    }
+}
